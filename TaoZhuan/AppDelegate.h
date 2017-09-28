03e7e81597ab4a1316a8881669be5ebf840322b1
jwpeng <jwpeng@amxc.tech>
2017-09-28 11:08:54 +0800

    Initial Commit

---
+++ TaoZhuan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaoZhuan
+//
+//  Created by 彭佳伟 on 2017/9/28.
+//  Copyright © 2017年 Jwpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
